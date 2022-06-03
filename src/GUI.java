6a0f7db84a20e66e8d081046e26ff550fde89c21
VakZok <100447067+VakZok@users.noreply.github.com>
2022-06-03 19:12:14 +0200

    First draft of this calculator
    To be renewed with arrays

--- src/GUI.java
+++ src/GUI.java
@@ -1,44 +1,72 @@
 import javax.swing.JFrame;
 import javax.swing.JButton;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI extends JFrame {
+public class GUI extends JFrame implements ActionListener {
+
+    //Buttons definieren
+    JButton addButton = new JButton();
+    JButton subtractButton = new JButton();
+    JButton multiplyButton = new JButton();
+    JButton divideButton = new JButton();
+    JButton moduloButton = new JButton();
+    JButton totalSumButton = new JButton();
+    JButton crossSumButton = new JButton();
+
+    //Interface Konstruktor
     public GUI(){
+        //Titel für Frame/GUI/Fenster festlegen
         super("Calculator");
 
-        //Buttons erzeugen
-        //Button "add"
-        JButton addButton = new JButton();
-        addButton.setBounds(50, 50, 50, 50);
-        //Button "add"
-        JButton subtractButton = new JButton();
-        subtractButton.setBounds(50, 50, 50, 50);
-        //Button "add"
-        JButton multiplyButton = new JButton();
-        multiplyButton.setBounds(50, 50, 50, 50);
-        //Button "add"
-        JButton divideButton = new JButton();
-        divideButton.setBounds(50, 50, 50, 50);
-        //Button "add"
-        JButton moduloButton = new JButton();
-        moduloButton.setBounds(50, 50, 50, 50);
-        //Button "add"
-        JButton totalSumButton = new JButton();
-        totalSumButton.setBounds(50, 50, 50, 50);
-        JButton crossSumButton = new JButton();
-        crossSumButton.setBounds(50, 50, 50, 50);
-
         //GUI(Frame) erzeugen
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(null);
-        setSize(500, 500);
-        setVisible(true);
-        //Buttons hinzufügen
-        add(addButton);
-//        add(subtractButton);
-//        add(multiplyButton);
-//        add(divideButton);
-//        add(moduloButton);
-//        add(totalSumButton);
-//        add(crossSumButton);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(null);
+        this.setSize(500, 500);
+        this.setVisible(true);
+
+        //Buttons im GUI implementieren
+        //add
+        addButton.setBounds(50, 50, 50, 50);
+        addButton.setText("+");
+        addButton.addActionListener(a -> System.out.println("+"));
+        //substract
+        subtractButton.setBounds(50, 50, 50, 50);
+        subtractButton.setText("-");
+        subtractButton.addActionListener(s -> System.out.println("-"));
+        //multiply
+        multiplyButton.setBounds(50, 50, 50, 50);
+        multiplyButton.setText("*");
+        multiplyButton.addActionListener(m -> System.out.println("*"));
+        //divide
+        divideButton.setBounds(50, 50, 50, 50);
+        divideButton.setText("/");
+        divideButton.addActionListener(d -> System.out.println("/"));
+        //module
+        moduloButton.setBounds(50, 50, 50, 50);
+        moduloButton.setText("%");
+        moduloButton.addActionListener(m -> System.out.println("%"));
+        //totalSum
+        totalSumButton.setBounds(50, 50, 50, 50);
+        totalSumButton.setText("q");
+        totalSumButton.addActionListener(t -> System.out.println("q"));
+        //crossSum
+        crossSumButton.setBounds(50, 50, 50, 50);
+        crossSumButton.setText("Σ");
+        crossSumButton.addActionListener(c -> System.out.println("Σ"));
+
+        //Buttons dem Frame (GUI) hinzufügen
+        this.add(addButton);
+        this.add(subtractButton);
+//        this.add(multiplyButton);
+//        this.add(divideButton);
+//        this.add(moduloButton);
+//        this.add(totalSumButton);
+//        this.add(crossSumButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Test");
     }
 }
