5d3ddcd732618dce5ebb65edc1f5dd820d1a99f2
VakZok <100447067+VakZok@users.noreply.github.com>
2022-06-04 20:43:29 +0200

    All panels added
    - functionButtons added to functionPanel, placed SOUTH
    
    Next step: sorting number buttons and adding ergebnis and (-)

--- src/Calculator.java
+++ src/Calculator.java
@@ -30,24 +30,21 @@ public class Calculator extends JFrame implements ActionListener {
         //und fügen es dem textPanel hinzu
         textPanel.add(textfield);
 
-        //Das erste Panel ist für unsere nummer von 0-9, das erg und negierte Werte (-). Es wird im Center liegen
-        JPanel numberPanel = new JPanel();
+        //Das zweite Panel ist für unsere nummer von 0-9, das erg und negierte Werte (-). Es wird im Center liegen
+        JPanel numberPanel = new JPanel(new GridLayout(4,3)); //wir geben dem panel ein Grid mit 4 Zeilen und 3 Spalte
         numberPanel.setBackground(Color.DARK_GRAY);
-        numberPanel.setLayout(new GridLayout(4,3)); //wir geben dem panel ein Grid mit 4 Zeilen und 3 Spalten
 
         //wir erzeugen die numberButtons, weisen Sie dem Array hinzugeben, geben ihnen einen ActionListener und fügen sie dem numberPanel hinzu
-        JButton[] numberButtons = new JButton[11];
+        JButton[] numberButtons = new JButton[12];
         for (int i=0; i<numberButtons.length-3; i++){ //
             numberButtons[i] = new JButton(String.valueOf(i)); //die Buttons werden mit dem Wert als Text erzeugt und dem Array hinzugefügt
             numberButtons[i].addActionListener(this); //ActionListener hinzufügen
             numberPanel.add(numberButtons[i]);
         }
 
-        //das zweite Panel ist für unsere Operatoren und wird sich rechts anordnen
-        JPanel operationPanel = new JPanel();
-        operationPanel.setLayout(new GridLayout(4,2)); //das operationPanel bekommt 4 Zeile und zwei Reihen
+        //das dritte Panel ist für unsere Operatoren und wird sich rechts anordnen
+        JPanel operationPanel = new JPanel(new GridLayout(4,2)); //das operationPanel bekommt 4 Zeile und zwei Reihen
         operationPanel.setBackground(Color.BLUE);
-        operationPanel.setVisible(true);
         //wir erzeugen unsere operationButtons
         JButton addButton = new JButton("+");
         JButton subtractButton = new JButton("-");
@@ -73,12 +70,24 @@ public class Calculator extends JFrame implements ActionListener {
             operationPanel.add(operationButton);
         }
 
-        //
+        //Das vierte und letzte Panel ist für unsere functionButtons und befindet sich unten
+        JPanel functionPanel = new JPanel(new GridLayout(1,2)); //functionPanel bekommt 1 Zeile und 2 Reihen
+        functionPanel.setBackground(Color.GREEN);
+        //nun erzeugen wir die functionButtons und weisen sie dem Array hinzu
+        JButton[] functionButtons = new JButton[2];
+        functionButtons[0] = new JButton("Delete");
+        functionButtons[1] = new JButton("Clear");
+        //wir geben ihnen ebenfalls einen actionListener und fügen sie dem functionPanel hinzu
+        for (JButton functionButton : functionButtons){
+            functionButton.addActionListener(this);
+            functionPanel.add(functionButton);
+        }
 
         //nun müssen wir die gewünschten Bauteile noch dem Fenster hinzufügen und es sichtbar machen
         window.add(textfield, BorderLayout.NORTH);
         window.add(numberPanel, BorderLayout.CENTER);
         window.add(operationPanel, BorderLayout.EAST);
+        window.add(functionPanel, BorderLayout.SOUTH);
         window.setVisible(true); //Frame sichtbar machen (erst am Ende, weil sonst Inhalte teilweise erst nach skalieren des Fensters erscheinen
     }
     public static void main(String[] args){
